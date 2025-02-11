3b9812ebd183a4270483426d032e3687e793de06
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2025-02-12 00:52:58 +0800

    update dependency

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.libzodiac.ui.elastic.elasticlib.Elastic;
+import frc.libzodiac.ui.Elastic;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
