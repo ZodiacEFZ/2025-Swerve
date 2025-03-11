6c0a93d3161ea25f247836e49c54fb445c2887af
Blue-Flag-666 <64064866+Blue-Flag-666@users.noreply.github.com>
2025-03-11 20:13:27 +0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -71,13 +71,6 @@ public class Robot extends TimedRobot {
 
         this.autonomousCommand = this.bot.getAutonomousCommand();
 
-        /*
-         * String autoSelected = SmartDashboard.getString("Auto Selector",
-         * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-         * = new MyAutoCommand(); break; case "Default Auto": default:
-         * autonomousCommand = new ExampleCommand(); break; }
-         */
-
         // schedule the autonomous command (example)
         if (this.autonomousCommand != null) {
             this.autonomousCommand.schedule();
