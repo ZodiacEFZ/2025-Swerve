1c797a60127735ad2f1c005b8ce39d32d934bf69
Blue-Flag-666 <64064866+Blue-Flag-666@users.noreply.github.com>
2025-03-13 09:05:28 +0800

    Update Arm

--- src/main/java/frc/robot/subsystem/ArmStage2.java
+++ src/main/java/frc/robot/subsystem/ArmStage2.java
@@ -4,11 +4,11 @@ import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -30,9 +30,10 @@ public class ArmStage2 extends SubsystemBase {
     private static final Angle L3_POSITION = Units.Degrees.of(13);
     private static final Angle L4_POSITION = Units.Degrees.of(80);
     private static final Angle ROTATION_HORIZONTAL = Units.Rotations.of(0);
-    private static final Angle ROTATION_VERTICAL = Units.Rotations.of(25);
-    private static final Angle ROTATION_HORIZONTAL_REVERSED = Units.Rotations.of(50);
-    private final MagEncoder encoder = new MagEncoder(27, -2950);
+    private static final Angle ROTATION_HORIZONTAL_REVERSED = Units.Rotations.of(
+            42.0 * 60 / 18 / 2);
+    private static final Angle ROTATION_VERTICAL = ROTATION_HORIZONTAL_REVERSED.div(2);
+    private final MagEncoder encoder = new MagEncoder(27, -3700);
     private final TalonFXMotor motor = new TalonFXMotor(28);
     private final SparkMaxMotor wrist = new SparkMaxMotor(24);
     private Angle position = null;
@@ -65,14 +66,17 @@ public class ArmStage2 extends SubsystemBase {
         this.motor.setSoftwareLimitSwitch(REVERSE_POSITION_LIMIT, FORWARD_POSITION_LIMIT);
 
         this.wrist.factoryDefault();
-        this.wrist.setInverted(false);
-        this.wrist.setPID(0.5, 0, 0.1);
+        this.wrist.setPID(0.5, 0, 0.5);
         var config = new SparkMaxConfig();
+        config.idleMode(SparkBaseConfig.IdleMode.kBrake);
+        config.inverted(true);
+        config.closedLoop.pid(0.5, 0, 0.5);
+        config.closedLoop.maxMotion.allowedClosedLoopError(0.1);
         config.closedLoop.maxMotion.maxAcceleration(6000).maxVelocity(4096);
         config.softLimit.forwardSoftLimitEnabled(true)
-                        .forwardSoftLimit(50)
+                        .forwardSoftLimit(ROTATION_HORIZONTAL_REVERSED.in(Units.Rotations))
                         .reverseSoftLimitEnabled(true)
-                        .reverseSoftLimit(0);
+                        .reverseSoftLimit(ROTATION_HORIZONTAL.in(Units.Rotations));
         this.wrist.applyConfiguration(config);
     }
 
@@ -82,14 +86,6 @@ public class ArmStage2 extends SubsystemBase {
 
     public void moveTo(Angle position) {
         this.motor.MotionMagicPosition(position);
-    }
-
-    public Command getRotateCommand(Angle position) {
-        return runOnce(() -> this.rotate(position));
-    }
-
-    public void rotate(Angle position) {
-        this.wrist.MAXMotionPosition(position);
     }
 
     public Command getRestoreCommand() {
@@ -105,21 +101,55 @@ public class ArmStage2 extends SubsystemBase {
 
     @Override
     public void initSendable(SendableBuilder builder) {
-        var chooser = new SendableChooser<Runnable>();
-        chooser.setDefaultOption("L4", this::L4);
-        chooser.addOption("L3", this::L3);
-        chooser.addOption("L2", this::L2);
-        chooser.addOption("L1", this::L1);
-        SmartDashboard.putData(chooser);
-        SmartDashboard.putData(runOnce(() -> chooser.getSelected().run()).withName("Run"));
+        SmartDashboard.putData("L4", this.getL4Command());
+        SmartDashboard.putData("L3", this.getL3Command());
+        SmartDashboard.putData("L2", this.getL2Command());
+        SmartDashboard.putData("L1", this.getL1Command());
         SmartDashboard.putData("Intake", this.getIntakeCommand());
         SmartDashboard.putData("Drop", this.getDropCommand());
         SmartDashboard.putData("Idle", this.getIdleCommand());
+
+        SmartDashboard.putData("Rotate Horizontal", this.getRotateCommand(ROTATION_HORIZONTAL));
+        SmartDashboard.putData("Rotate Vertical", this.getRotateCommand(ROTATION_VERTICAL));
+        SmartDashboard.putData("Rotate Horizontal Reversed",
+                               this.getRotateCommand(ROTATION_HORIZONTAL_REVERSED));
+    }
+
+    public Command getL4Command() {
+        return runOnce(this::L4);
+    }
+
+    public Command getL3Command() {
+        return runOnce(this::L3);
+    }
+
+    public Command getL2Command() {
+        return runOnce(this::L2);
+    }
+
+    public Command getL1Command() {
+        return runOnce(this::L1);
+    }
+
+    public Command getIntakeCommand() {
+        return runOnce(this::intake);
+    }
+
+    public Command getDropCommand() {
+        return runOnce(this::drop);
+    }
+
+    public Command getIdleCommand() {
+        return runOnce(this::idle);
+    }
+
+    public Command getRotateCommand(Angle position) {
+        return runOnce(() -> this.rotate(position));
     }
 
     public void L4() {
         this.moveTo(L4_POSITION);
-        this.rotate(ROTATION_HORIZONTAL);
+        this.rotate(ROTATION_HORIZONTAL_REVERSED);
         this.position = L4_POSITION;
     }
 
@@ -141,18 +171,6 @@ public class ArmStage2 extends SubsystemBase {
         this.position = L1_POSITION;
     }
 
-    public Command getIntakeCommand() {
-        return runOnce(this::intake);
-    }
-
-    public Command getDropCommand() {
-        return runOnce(this::drop);
-    }
-
-    public Command getIdleCommand() {
-        return runOnce(this::idle);
-    }
-
     public void intake() {
         this.moveTo(INTAKE_POSITION);
         this.rotate(ROTATION_VERTICAL);
@@ -172,24 +190,12 @@ public class ArmStage2 extends SubsystemBase {
 
     public void idle() {
         this.moveTo(REVERSE_POSITION_LIMIT);
-        this.rotate(ROTATION_VERTICAL);
+        this.rotate(ROTATION_HORIZONTAL);
         this.position = null;
     }
 
-    public Command getL4Command() {
-        return runOnce(this::L4);
-    }
-
-    public Command getL3Command() {
-        return runOnce(this::L3);
-    }
-
-    public Command getL2Command() {
-        return runOnce(this::L2);
-    }
-
-    public Command getL1Command() {
-        return runOnce(this::L1);
+    public void rotate(Angle position) {
+        this.wrist.MAXMotionPosition(position);
     }
 
     public Collection<TalonFX> getTalonFXMotors() {
